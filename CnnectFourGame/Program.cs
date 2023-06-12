02951f50b612d90253dac5af7adc2164f6367bfb
Ragde010 <122415919+Ragde010@users.noreply.github.com>
2023-06-12 13:04:24 -0600

    Update Program.cs
    Add some comments and  looping inside the function

--- CnnectFourGame/Program.cs
+++ CnnectFourGame/Program.cs
@@ -2,25 +2,25 @@
 public class Cell
 {
     public char Symbol { get; set; }
-
+    //Constructor
     public Cell(char symbol)
     {
         Symbol = symbol;
     }
 }
-
+// Base Class
 public abstract class BoardGame
 {
-    protected const int Rows = 6;
-    public const int Columns = 7;
-    protected Cell[,] Cells;
+    protected const int Rows = 6; //It represents the number of rows in the game board.
+    public const int Columns = 7; //It represents the number of column in the game board.
+    protected Cell[,] Cells; // It represents the game board and hold the cells of the board as a 2D array of Cell object.
 
-    public abstract void InitializeBoard();
-    public abstract void PrintBoard();
-    public abstract bool IsValidMove(int column);
-    public abstract void DropSymbol(int column, char symbol);
-    public abstract bool CheckWin(char symbol);
-    public abstract bool IsBoardFull();
+    public abstract void InitializeBoard(); //It is resposible for initializing the game board with empty cells or any specific intial state
+    public abstract void PrintBoard(); //It is responsible for printing the current state of the game board to the console
+    public abstract bool IsValidMove(int column); //It is responsible for checking if the move is valid for the given column on the board game.
+    public abstract void DropSymbol(int column, char symbol);//It is responsible for dropping a symbol into a specified column on the board game.
+    public abstract bool CheckWin(char symbol); //It is responsible for checking if a player with specified symbol has won the game.
+    public abstract bool IsBoardFull(); //It is responsible for checking if the game board is full (no empty cells remaining)
 }
 
 public class ConnectFour : BoardGame
@@ -29,18 +29,21 @@ public class ConnectFour : BoardGame
     {
         Cells = new Cell[Rows, Columns];
     }
-
+    //This function overrides the InitializeBoard method inherited from the BoardGame class.
+    //It initializes the game board by iterating over each cell and setting it to a new Cell object with a symbol of empty space (' ').
     public override void InitializeBoard()
     {
         for (int row = 0; row < Rows; row++)
         {
             for (int col = 0; col < Columns; col++)
             {
-                cells[row, col] = new Cell(' ');
+                Cells[row, col] = new Cell(' ');
             }
         }
     }
-
+    //This function overrides the PrintBoard method inherited from the BoardGame class. 
+    //It prints the current state of the game board to the console. 
+    //It iterates over each cell and prints its symbol, along with surrounding formatting characters.
     public override void PrintBoard()
     {
         Console.Clear();
@@ -57,12 +60,13 @@ public class ConnectFour : BoardGame
         PrintColumnNumbers();
         Console.WriteLine();
     }
-
-    private char GetCellSymbol(int row, int col)
+    //This private helper function returns the symbol of the cell at the specified row and column of the board game.
+    private char GetCellSymbol(int row, int col) 
     {
         return Cells[row, col].Symbol;
     }
-
+    // This private helper function prints the column numbers above the board game.
+    //It iterates over the columns and prints the column numbers with proper spacing.
     private void PrintColumnNumbers()
     {
         for (int col = 0; col < Columns; col++)
@@ -74,7 +78,11 @@ public class ConnectFour : BoardGame
 
     public override bool IsValidMove(int column)
     {
-        
+        if(column < 0 || column >= Columns)
+        {
+            return false;
+        }
+        return Cells[0,column].Symbol == '';
     }
 
     public override void DropSymbol(int column, char symbol)
@@ -84,7 +92,7 @@ public class ConnectFour : BoardGame
 
     public override bool CheckWin(char symbol)
     {
-        // Check rows for a win
+        // Check rows/column for a win
         
        
     }
